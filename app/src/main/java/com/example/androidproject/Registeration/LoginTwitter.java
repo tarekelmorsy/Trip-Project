0a0cee1af818a153deea61d5108485ed4c53f9b9
AyaAlaa0109 <ayahalaa010@gmail.com>
2022-01-21 21:43:18 +0200

    login facebook and sign out

--- app/src/main/java/com/example/androidproject/Registeration/LoginTwitter.java
+++ app/src/main/java/com/example/androidproject/Registeration/LoginTwitter.java
@@ -1,14 +1,16 @@
-package com.example.androidproject;
+package com.example.androidproject.Registeration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.androidproject.R;
+
+public class LoginTwitter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_twitter);
     }
 }
--- app/src/main/java/com/example/androidproject/Registeration/SplashScreen.java
+++ app/src/main/java/com/example/androidproject/Registeration/SplashScreen.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.androidproject.MainActivity;
 import com.example.androidproject.R;
 
 public class SplashScreen extends AppCompatActivity {
@@ -14,6 +13,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         Thread background = new Thread() {
             public void run() {
                 try {
