b82995628c4a37d018d5df7f4c3cc6186a82f3f0
AyaAlaa0109 <ayahalaa010@gmail.com>
2022-01-22 00:00:49 +0200

    main1

--- app/src/main/java/com/example/androidproject/Registeration/LoginEmail.java
+++ app/src/main/java/com/example/androidproject/Registeration/LoginEmail.java
@@ -1,9 +1,9 @@
 package com.example.androidproject.Registeration;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -12,66 +12,114 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidproject.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginEmail extends AppCompatActivity {
-   Button btLoginEmail;
-   TextInputEditText et_email,et_password;
+    Button btLoginEmail;
+    TextInputEditText et_email, et_password, et_ConfirmPass;
+    final String TAG = "LoginEmail";
 
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
-    FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_email);
         init();
-
+        firebaseAuth = FirebaseAuth.getInstance();
         btLoginEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email=et_email.getText().toString();
-                String password=et_password.getText().toString();
-
-                register(email,password);
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                register(email, password);
+                et_email.setText("");
+                et_password.setText("");
+                et_ConfirmPass.setText("");
             }
         });
-
-
-
-
-
-    }
-    public void init(){
-
-        btLoginEmail=findViewById(R.id.bt_login_email);
-        et_email=findViewById(R.id.et_login_password_email);
-        et_password=findViewById(R.id.et_login_email);
-
-
     }
 
 
-    public void register(String email,String password){
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(LoginEmail.this,"registered: "+email+" "+password,Toast.LENGTH_LONG).show();
+
+
+    public void init() {
+
+        btLoginEmail = findViewById(R.id.bt_login_email);
+        et_email = findViewById(R.id.et_login_email);
+        et_password = findViewById(R.id.et_login_password_email);
+        et_ConfirmPass = findViewById(R.id.et_login_conifirmpassword_email);
+    }
+
+
+    public void register(String email, String password) {
+        String confirmPassword=et_ConfirmPass.getText().toString();
+
+        if(email.equals("")||password.equals("")||confirmPassword.equals("")){
+
+            Toast.makeText(LoginEmail.this, "please fill all data", Toast.LENGTH_LONG).show();
+
+
+        }
+
+       else{
+
+        if (confirmPassword.equals(password)) {
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user=firebaseAuth.getCurrentUser();
+                        user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(LoginEmail.this, "go to your  mail to verify your account to login", Toast.LENGTH_SHORT).show();
 
 
 
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: "+e.getMessage());
+                                Toast.makeText(LoginEmail.this, "verification mail not sent  "+ e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+
+
+
+                        Toast.makeText(LoginEmail.this, "account created", Toast.LENGTH_LONG).show();
+//intent for main menue
+
+
+                    } else {
+                        String errorMessage = task.getException().getLocalizedMessage();
+                        Toast.makeText(LoginEmail.this, errorMessage, Toast.LENGTH_LONG).show();
+                        Log.i(TAG, "onComplete: " + errorMessage);
+                    }
                 }
-                else{
-                    Toast.makeText(LoginEmail.this,"account not created",Toast.LENGTH_LONG).show();
+            });
+
+        }
 
 
-                }
-            }
-        });
+else{
+            Toast.makeText(LoginEmail.this, "confirm password not equal  password", Toast.LENGTH_LONG).show();
+
 
     }
 
+
+
+}}
 }
---
+++ app/src/main/java/com/example/androidproject/data/Trip.java
@@ -0,0 +1,103 @@
+package com.example.androidproject.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trip {
+    private String tripName;
+    private String startPoint;
+    private String endPoint;
+    private String dat;
+    private String alarm;
+    private String repeat;
+    private String way;
+    private String notes;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    Trip (){}
+    public String getTripName() {
+        return tripName;
+    }
+
+    public void setTripName(String tripName) {
+        this.tripName = tripName;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public String getDat() {
+        return dat;
+    }
+
+    public void setDat(String dat) {
+        this.dat = dat;
+    }
+
+    public String getAlarm() {
+        return alarm;
+    }
+
+    public void setAlarm(String alarm) {
+        this.alarm = alarm;
+    }
+
+    public String getRepeat() {
+        return repeat;
+    }
+
+    public void setRepeat(String repeat) {
+        this.repeat = repeat;
+    }
+
+    public String getWay() {
+        return way;
+    }
+
+    public void setWay(String way) {
+        this.way = way;
+    }
+
+    public Trip(String name, String start, String end, String dat, String alarm, String repeat, String way) {
+        this.tripName = name;
+        this.startPoint = start;
+        this.endPoint = end;
+        this.dat = dat;
+        this.alarm = alarm;
+        this.repeat = repeat;
+        this.way = way;
+    }
+
+
+}
